65ca66c7858ea94804dbfd53b91a9bdc4c7f419a
ArcticJunkie <pikka99@gmail.com>
2021-01-06 17:36:01 +0530

    added top level layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 
 function App() {
   return (
-    <div className="App">
-      abcdefg
+    <div className="flex flex-col">
+      <nav className="">
+        top nav
+      </nav>
+      <div className="flex h-screen">
+        <nav className="py-8 px-4 bg-blue-50">
+          left nav
+        </nav>
+        <div className="bg-red-100 flex-grow">
+          main content
+        </div>
+      </div>
     </div>
   );
 }
